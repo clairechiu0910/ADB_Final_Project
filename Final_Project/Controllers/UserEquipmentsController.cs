c402b704a7f660124b5f1aae6bb73334a11458a4
clairechiu0910 <hopemiracle1997@gmail.com>
2021-01-17 17:21:04 +0800

    GetUserEquipments (Json)

--- Final_Project/Controllers/UserEquipmentsController.cs
+++ Final_Project/Controllers/UserEquipmentsController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
 using Final_Project.Repositories.Interface;
 
 
 namespace Final_Project.Controllers
 {
+    [Authorize]
     public class UserEquipmentsController : Controller
     {
         private readonly IEquipmentRepo _equipmentRepo;
@@ -22,5 +25,11 @@ namespace Final_Project.Controllers
             return View();
         }
 
+        public IActionResult GetUserEquipments()
+        {
+            var username = HttpContext.Session.GetString("UserName");
+            var equipments = _equipmentRepo.GetUserEquipment(username);
+            return Json(equipments);
+        }
     }
 }
